93261931c8649756d93bc707f25349848b23a4e2
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 03:25:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,7 +38,7 @@ def get_english_meaning(file_name, emoji)
       if value == emoji
         return emotion
       else 
-        return "Sorry, that emoticon was not found"
+        "Sorry, that emoticon was not found"
       end
     end
   end  
