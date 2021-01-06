41b7d7d6a4ddc092dd4c70e66a852395ea8439ec
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 03:38:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ def get_japanese_emoticon(file_name, emoji)
     emojis.each do |emotion, symbol|
     symbol.each do |lang, value|
       if value == emoji
-        return lang[value][1]
+        return symbol[1]
       end
     end
   end  
